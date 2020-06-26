45f3987afc0556e5227b571f77e904db8ee99e92
ashvini <deepabharath0306@gmail.com>
2020-06-26 12:18:12 -0700

    "Initial Project Set up

---
+++ ElasticSearchJpa/src/main/java/com/es/ElasticSearchJpaApplication.java
@@ -0,0 +1,13 @@
+package com.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSearchJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticSearchJpaApplication.class, args);
+	}
+
+}
