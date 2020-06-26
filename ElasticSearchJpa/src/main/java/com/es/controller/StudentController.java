4af028700878f1ee619e86a8ade85cc4f4f41a34
ashvini <deepabharath0306@gmail.com>
2020-06-26 16:35:40 -0700

    UPdated student controller class file

--- ElasticSearchJpa/src/main/java/com/es/controller/StudentController.java
+++ ElasticSearchJpa/src/main/java/com/es/controller/StudentController.java
@@ -1,45 +1,42 @@
 package com.es.controller;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.es.model.Student;
 import com.es.repository.StudentRepository;
 
 @RestController
+@RequestMapping("/s")
 public class StudentController {
 
 	@Autowired
-	private StudentRepository studentrepo;
+	private StudentRepository sRepo;
 	
 	@PostMapping("/add")
 	public void addStudent(Student s) {
 		System.out.println("Isnide the add student()");
-		studentrepo.save(s);
+		sRepo.save(s);
 		 }	
 	
-	@GetMapping("/findbyid/{id}")
-	public  Optional<Student> findById(@PathVariable int id) {
-		System.out.println("Isnide the add student()");
-		return studentrepo.findById(id);
-	}
-	
-	@GetMapping("/findbyname/{name}")
-	public  List<Student> findByName(@PathVariable String name) {
-		System.out.println("Isnide the add student()");
-		return studentrepo.findByName(name);
-	}
-	
-	@GetMapping("/findbysubject/{subject}")
-	public  List<Student> findBySubject(@PathVariable String subject) {
-		System.out.println("Isnide the add student()");
-		return studentrepo.findBySubject(subject);
-	}
-	
+//	@GetMapping("/findbyid/{id}")
+//	public  Optional<Student> findById(@PathVariable int id) {
+//		System.out.println("Isnide the add student()");
+//		return studentrepo.findById(id);
+//	}
+//	
+//	@GetMapping("/findbyname/{name}")
+//	public  List<Student> findByName(@PathVariable String name) {
+//		System.out.println("Isnide the add student()");
+//		return studentrepo.findByName(name);
+//	}
+//	
+//	@GetMapping("/findbysubject/{subject}")
+//	public  List<Student> findBySubject(@PathVariable String subject) {
+//		System.out.println("Isnide the add student()");
+//		return studentrepo.findBySubject(subject);
+//	}
+//	
 }
--- ElasticSearchJpa/src/main/java/com/es/model/Student.java
+++ ElasticSearchJpa/src/main/java/com/es/model/Student.java
@@ -1,10 +1,11 @@
 package com.es.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Student {
-	
+	@Id
 	private int id;
 	private String studentName;
 	private int marks;
